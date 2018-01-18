3e7284124146d7be177d80cad1a90ef628c7cebb
James Brennan <james@jamesbrennan.ca>
2018-01-17 16:11:56 -0800

    Fix sha

--- iidy.rb
+++ iidy.rb
@@ -4,7 +4,7 @@ class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
   url 'https://github.com/unbounce/iidy/releases/download/v1.6.0/iidy-macos-amd64.zip'
   version '1.6.0'
-  sha256 'd98bcf13f5e05701402377a26ab7fa0a20f7a6bae53c97497aca30971487071b'
+  sha256 '2107e0b0328ce13a7830a69bf9e0fedfa13dccc62480db0fbf1cd9e2c27ec001'
 
   depends_on :arch => :x86_64
 
