4710f04af6802833b69c80f54aa902b57567c811
James Brennan <james@jamesbrennan.ca>
2018-01-17 16:05:49 -0800

    iidy 1.6.0

--- iidy.rb
+++ iidy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
-  url 'https://github.com/unbounce/iidy/releases/download/v1.6.0-rc7/iidy-macos-amd64.zip'
-  version '1.6.0-rc7'
+  url 'https://github.com/unbounce/iidy/releases/download/v1.6.0/iidy-macos-amd64.zip'
+  version '1.6.0'
   sha256 'd98bcf13f5e05701402377a26ab7fa0a20f7a6bae53c97497aca30971487071b'
 
   depends_on :arch => :x86_64
