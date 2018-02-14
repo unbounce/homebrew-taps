f6e7c25493a3f7a5b745b6563658ee52400e6247
James Brennan <james@jamesbrennan.ca>
2018-02-14 11:12:45 -0800

    Fix iidy 1.6.4 url

--- iidy.rb
+++ iidy.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
-  url 'https://github.com/unbounce/iidy/releases/download/v1.6.3/iidy-macos-amd64.zip'
+  url 'https://github.com/unbounce/iidy/releases/download/v1.6.4/iidy-macos-amd64.zip'
   version '1.6.4'
   sha256 '413c6f1ce48d2819a79b3b2123b37ed654d7084340d5c0118011471aca8b2ce9'
 
