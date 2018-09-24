a7b1494cb7ab2d7100cb0397df1e1bb47b2552a1
James Brennan <james@jamesbrennan.ca>
2018-09-24 10:08:39 -0700

    Upgrade to iidy 1.6.6-rc3

--- iidy.rb
+++ iidy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
-  url 'https://github.com/unbounce/iidy/releases/download/v1.6.6-rc2/iidy-macos-amd64.zip'
-  version '1.6.6-rc2'
+  url 'https://github.com/unbounce/iidy/releases/download/v1.6.6-rc3/iidy-macos-amd64.zip'
+  version '1.6.6-rc3'
   sha256 'a1e0743d43ed9eecb2265808430e7ea0c53ffe03f1deeb4773c7e0cca4279fc4'
 
   depends_on :arch => :x86_64
