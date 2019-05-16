42fdac885d081a9e2357f68b96aa90a558c341c2
Tavis Rudd <tavis@damnsimple.com>
2019-05-16 14:58:08 -0700

    Fix wrong version field for iidy

--- iidy.rb
+++ iidy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
   url 'https://github.com/unbounce/iidy/releases/download/v1.9.0/iidy-macos-amd64.zip'
-  version '1.8.0'
+  version '1.9.0'
   sha256 '132596ee55c9dff70020246a2d26cbdb8c1edcac43a99b573c2d7f7439f20f22'
 
   depends_on :arch => :x86_64
