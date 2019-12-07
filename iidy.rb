69b4b75bd949b25e287dabce019efea1d8389066
Tavis Rudd <tavis@damnsimple.com>
2019-12-07 15:26:24 -0800

    bump iidy to v1.10.0

--- iidy.rb
+++ iidy.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Iidy < Formula
   homepage 'https://github.com/unbounce/iidy'
-  url 'https://github.com/unbounce/iidy/releases/download/v1.9.0/iidy-macos-amd64.zip'
-  version '1.9.0'
-  sha256 '132596ee55c9dff70020246a2d26cbdb8c1edcac43a99b573c2d7f7439f20f22'
+  url 'https://github.com/unbounce/iidy/releases/download/v1.10.0/iidy-macos-amd64.zip'
+  version '1.10.0'
+  sha256 '1a797a9e813cf76964081bffd74230cc20582336a142662a40234bcd7f6c8c02'
 
   depends_on :arch => :x86_64
 
