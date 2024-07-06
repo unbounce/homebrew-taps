baf2d9f88a1e9eec809995450ce50966e4d582e4
Kadeem Hassam <kadeem@unbounce.com>
2024-07-05 23:33:07 -0700

    PS-3746 Updates iidy to 1.13.0

--- iidy.rb
+++ iidy.rb
@@ -1,12 +1,20 @@
 require 'formula'
 
 class Iidy < Formula
-  homepage 'https://github.com/unbounce/iidy'
-  url 'https://github.com/unbounce/iidy/releases/download/v1.12.0/iidy-macos-amd64.zip'
-  version '1.12.0'
-  sha256 '2c6dd182edc023c18882eb9ede74607cd7bebee7a644864fd8fbbcfcbdcf17d1'
+  os = OS.linux? ? 'linux' : 'macos'
+  arch = Hardware::CPU.arm? ? 'arm' : 'amd'
 
-  depends_on :arch => :x86_64
+  checksums = {
+    'linux-amd' => 'd881e46de45348e7d3ffe22de682fa880ee2ae43b7c524ee2d49b70050454ca2',
+    'linux-arm' => '211f6280d59cf4fb3b931ccf5674110495b4972f0f27c0fb314cdb2452393607',
+    'macos-amd' => '97baa3b91bdcefc3c5487e11f51fc6626adf698e8474e2e952b220acdbac244f',
+    'macos-arm' => 'e0a7d940e83564c1dededbb7e992274a29943f0176d0ba51f3d97291781eff8a',
+  }
+
+  homepage 'https://github.com/unbounce/iidy'
+  url 'https://github.com/unbounce/iidy/releases/download/v1.13.0/iidy-' + os + '-' + arch + '64.zip'
+  version '1.13.0'
+  sha256 checksums[os + '-' + arch]
 
   def install
     bin.install 'iidy'
